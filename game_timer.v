49cb1818ae612ca7cf874b7b28afd95c08434bfe
Galen Carter <156123988+axionenthusiast@users.noreply.github.com>
2026-04-23 21:14:18 -0400

    reconfiguration of top + added mole func

--- game_timer.v
+++ game_timer.v
@@ -24,40 +24,22 @@ module game_timer(
 
     input clock,
     input rst,
-    output [6:0] cathode,
-    output [7:0] anode,
-    output reg game_over
+    output reg game_over,
+    output [11:0] bcd
     );
     
     wire seconds_clock; // 1 Hz
-    wire fsm_clock;     // 1 kHz
-    reg [6:0] counter = 7'd90;
-    wire [11:0] bcd;
-    
+    reg [6:0] counter = 7'd90;    
         
     clock_divider cd (
         .in_clk(clock),
         .out_clk(seconds_clock)
     );
     
-    
-    faster_clock_divider fcd (
-        .in_clk(clock),
-        .out_clk(fsm_clock)
-    );
-    
     binary_to_BCD_converter timer_bcd (
         .bin(counter),
         .bcd(bcd)
     );
-    
-    fsm timer_fsm (
-        .clock(fsm_clock),
-        .sixteen_bit_number({4'd0, bcd}),
-        .cathode(cathode),
-        .anode(anode)
-    );
-    
     
     always @(posedge seconds_clock or posedge rst) begin
     if (rst) begin
